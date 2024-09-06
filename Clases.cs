f7d07410e4d2fe270b7aff1e7977bcd56db2f443
unknown <karlontxo@gmail.com>
2024-09-06 15:52:49 -0300

    hasta insertar BD

---
+++ Clases.cs
@@ -0,0 +1,122 @@
+using MySql.Data.MySqlClient;
+using System.Data;
+
+
+namespace libreriaClase{
+
+    class Persona
+    {
+
+//        private string nombre;
+
+
+        public string Apellido {get;set;}
+        public string Nombre {get;set;}
+        public int DNI {get;set;}
+        public string FechaNacimiento {get;set;}
+
+
+       
+
+        public  Persona(string rApellido,string rNombre, string rFechaNacimiento, int rDni){
+            Apellido = rApellido;
+            Nombre = rNombre;
+            FechaNacimiento = rFechaNacimiento;
+            DNI = rDni;
+
+        }
+
+        public Persona(){
+            
+        }
+        public string caminar(){
+
+            return "Caminando....";
+
+        }
+
+        public void mostrarPersona(){
+            Console.WriteLine("Apellido: {0}", Apellido);
+            Console.WriteLine("Nombre: {0}", Nombre);
+            Console.WriteLine("Fecha Nacimiento: {0}", FechaNacimiento);
+            Console.WriteLine("DNI: {0}", DNI);
+        }
+
+        public void devolverEdad(){
+            Console.WriteLine("La edad es:.....");
+        }
+
+    }
+
+
+    class Alumno : Persona {
+
+        private int legajo;
+        private string curso;
+
+        private int[] notas;
+
+
+        public void estudiar(){
+            Console.WriteLine("Estudiando....");
+        }
+
+        public void devolverCurso(){
+            Console.WriteLine("El curso es: {0}", this.curso);
+        }
+    }
+
+
+
+    class conexionBD{
+
+        MySqlConnection Conector; //objeto   atributos?
+
+        MySqlCommand Comando;
+
+        public void conectar (){ //metodo
+
+                //Conector = new MySqlConnection(@"server=127.0.0.1;database=5to_escuela;Uid=5to_agbd;pwd=Triggers!");
+                Conector=new MySqlConnection(@"server=127.0.0.1;database=5to_escuela;Uid=root;pwd=123456789");
+
+                Comando = Conector.CreateCommand();
+
+        }
+
+        public void insertarBD (Persona rPersona) {
+
+            //Comando.CommandText = "insert into Persona (DNI,Apellido,Nombre,FechaNacimiento) values (1233246,'Sanchez','Miguel',' 21.05.2015') ";
+            
+            Comando.CommandText = "insert into Persona (DNI,Apellido,Nombre,FechaNacimiento) values ('" 
+
+            + rPersona.DNI +
+            "', '"
+            + rPersona.Apellido +
+            "', '"
+            + rPersona.Nombre +
+            "', '"
+            + rPersona.FechaNacimiento + "')";
+            
+            Comando.CommandType = CommandType.Text;  //va siempre asi
+            Conector.Open();
+            Comando.ExecuteNonQuery();
+            Conector.Close();
+            
+        }
+
+        public void mostrarEscalar(){
+
+            string sql ="select Apellido from Persona where DNI = '5464645'";
+            Comando.CommandText = sql;
+            Comando.CommandType = CommandType.Text;
+            Conector.Open();
+            object datoDevuelto = Comando.ExecuteScalar();
+            Conector.Close();
+
+            Console.WriteLine (datoDevuelto);
+            Console.ReadKey();
+        }
+
+
+    }
+} 
---
+++ Principal.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Security.AccessControl;
+using System.Collections.Generic;  //librería de LIST
+using libreriaClase;
+
+namespace libreria
+{
+
+    class Pricipal{
+
+        
+
+        static void Main(){
+
+            List<Persona> listaPersona = new List<Persona>();
+
+            string opcion = mostrarMenu();
+
+            while ( opcion != "9") {
+
+
+                switch(opcion) 
+                            {
+                            case "1":
+                                listaPersona.Add(crearPersona());
+                                break;
+                            case "2":
+                                mostrarPersona(listaPersona);
+                                break;
+                            case "3":
+                                buscarPersona(listaPersona);
+                                break;
+                            case "4":
+                                insertarPersona(listaPersona);
+                                break;
+                            /*case "5":
+                                mostrarEscalar
+*/
+                            default:
+                                Console.WriteLine("Opción Incorrecta...");
+                                Console.ReadKey();
+                                break;
+                            }
+
+            opcion = mostrarMenu();
+
+            }
+
+        }
+
+         public static Persona crearPersona(){
+
+            Console.Clear();
+            Persona p1 = new Persona();
+
+            Console.WriteLine("Ingreso de Personas");
+            Console.WriteLine("-------------------");
+            Console.WriteLine("");
+
+            Console.Write("Ingrese Apellido: ");
+            p1.Apellido = Console.ReadLine();
+
+            Console.Write("Ingrese Nombre: ");
+            p1.Nombre = Console.ReadLine();
+
+            Console.Write("Ingrese DNI: ");
+            p1.DNI = Convert.ToInt32(Console.ReadLine());            
+            
+
+            Console.Write("Ingrese Fecha Nacimiento: ");
+            p1.FechaNacimiento = Console.ReadLine();
+
+            return p1;
+
+        }
+
+        public static void mostrarPersona(List<Persona> personas){
+                Console.Clear();
+                foreach(var elem in personas){
+                    
+                    Console.Write("Apellido: ");
+                    Console.WriteLine(elem.Apellido);
+                    Console.Write("Nombre: ");
+                    Console.WriteLine(elem.Nombre);
+                        Console.Write("DNI:");
+                        Console.WriteLine(elem.DNI);
+                        Console.Write("Fecha Nacimiento:");
+                        Console.WriteLine(elem.FechaNacimiento);
+                        
+                    //Console.WriteLine("");
+                    Console.WriteLine("-------*-*-------");
+                    //Console.WriteLine(("").PadRight(24, '-'));
+
+                    
+                }
+                Console.ReadKey();
+        }
+
+        public static void buscarPersona(List<Persona> personas){
+                int dni;
+                int flag=0;
+                Console.Clear();
+                Console.Write("Ingrese DNI Persona a buscar: ");
+                dni = Convert.ToInt32(Console.ReadLine());
+
+                foreach(var elem in personas){
+
+                    if ( elem.DNI == dni ){
+
+                        Console.Write("Apellido: ");
+                        Console.WriteLine(elem.Apellido);
+                        Console.Write("Nombre: ");
+                        Console.WriteLine(elem.Nombre);
+                        Console.Write("DNI:");
+                        Console.WriteLine(elem.DNI);
+                        Console.Write("Fecha Nacimiento:");
+                        Console.WriteLine(elem.FechaNacimiento);
+                        Console.WriteLine("----------------");
+
+                        Console.ReadKey();
+                        flag = 1;
+
+                    }
+
+                }
+                if (flag == 0){
+                    Console.WriteLine("Registro No Encontrado");
+                    Console.ReadKey();
+                }
+    }
+
+        public static void insertarPersona(List<Persona> rlistaPersona) {
+        
+            conexionBD con = new conexionBD();
+
+            con.conectar();
+
+            foreach (var p in rlistaPersona) {
+
+                con.insertarBD(p);
+
+            }
+
+            Console.WriteLine (" Registros insertados...");
+            Console.WriteLine ("Presione una tecla para continuar <3");
+            Console.ReadKey();
+            
+        }
+
+
+
+
+
+
+        public static string mostrarMenu(){
+
+            string opcion;
+
+            Console.Clear();
+
+           
+            Console.WriteLine("- Menú -");
+            Console.WriteLine("--------");
+
+            Console.WriteLine("");
+            Console.WriteLine("1.- Crear Alumno");
+            Console.WriteLine("2.- Mostrar Alumnos");
+            Console.WriteLine("3.- Buscar Alumno");
+            Console.WriteLine("4.- Exportar alumnos a BD");
+
+            Console.WriteLine("");
+            Console.BackgroundColor = ConsoleColor.Blue; 
+            Console.Write("Elija una opción: ");
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            opcion = Console.ReadLine();
+
+            return opcion;
+
+
+        }
+    }
+
+
+    
+}
